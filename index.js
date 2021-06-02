b94aa5c1c1b21fb026110414d5be4e680fadbf73
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-06-02 20:36:36 +0600

    multiple action, one reducer, one store

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const createStore = redux.createStore;
 
 // action is a object and action creator function return a object
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
 function buyCake(){
     return {
@@ -11,12 +12,19 @@ function buyCake(){
     }
 }
 
+function buyIceCream(){
+    return {
+        type: BUY_ICE_CREAM,
+    }
+}
+
 
 // reducer -> function -> processing action
 // (previous state, action) = return newSate
 
 const initialState = {
-    numberOfCakes: 10
+    numberOfCakes: 10,
+    numberOfIceCream: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -24,6 +32,11 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return{
             ...state,
             numberOfCakes: state.numberOfCakes - 1
+        }
+
+        case BUY_ICE_CREAM: return{
+            ...state,
+            numberOfIceCream: state.numberOfIceCream - 1
         }
 
         default: return state;
@@ -38,4 +51,6 @@ const unsubscribe = store.subscribe(()=> console.log("Update state : ", store.ge
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
